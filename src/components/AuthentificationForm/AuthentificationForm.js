5789a3c4dc0cd23e15b4e37049cd6e3f016b7fa8
SostellTODA <sostelltoda@gmail.com>
2023-09-26 11:19:12 +0200

    review formulaire

--- src/components/AuthentificationForm/AuthentificationForm.js
+++ src/components/AuthentificationForm/AuthentificationForm.js
@@ -17,7 +17,6 @@ const AuthentificationForm = () => {
   <CloseButton
             className="absolute top-4 left-4 text-gray-700 hover:bg-gray-200 active:bg-gray-400 p-1 rounded-full w-10 h-10 flex items-center justify-center"
             onClick={() => {
-              setError(null);
               closeModal();  
             }}
           />
@@ -29,32 +28,32 @@ const AuthentificationForm = () => {
       />
     </div>
     <form className="w-full">
-          <div className="py-1.5 mx-auto max-w-xl flex">
+          <div className="py-4 mx-auto max-w-xl flex">
             <input type="radio" name="role_choice" id="artist" /><p className="font-bold">Je suis artiste</p> : je souhaite partager mes créations.
           </div>
-          <div className="py-1.5 mx-auto max-w-xl flex">
+          <div className="py-4 mx-auto max-w-xl flex">
             <input type="radio" name="role_choice" id="user" /><p className="font-bold">Je suis amateur d'art</p> : je souhaite rejoindre la communauté.
           </div>
-          <div className="grid grid-cols-2 max-w-md mx-auto py-3.5 ">
+          <div className="grid grid-cols-2 max-w-md mx-auto py-6">
           <input type="text" placeholder="Nom" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           <input type="text" placeholder="Prénom" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           </div>
-          <div className="grid grid-cols-2 max-w-md mx-auto py-1.5">
+          <div className="grid grid-cols-2 max-w-md mx-auto py-6">
           <p className="text-gray-500">Date de naissance</p>
           <input type="text" placeholder="JJ/MM/AAAA" id="birthdate" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           </div>
-          <div className="grid grid-cols-2 max-w-md mx-auto py-1.5">
+          <div className="grid grid-cols-2 max-w-md mx-auto py-6">
           <input type="text" placeholder="Ville" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           <input type="text" placeholder="Pays" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           </div>
-          <div className="max-w-xs mx-auto py-1.5 border-black pb-4 border-b-2 flex justify-start">
-              <input type="text" placeholder="Email" className="bg-gray-200 placeholder-gray-500 py-1.5"/>
+          <div className="max-w-xs mx-auto py-1.5  flex justify-start">
+              <input type="text" placeholder="Email" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4"/>
           </div>
           <div className="grid grid-cols-2 max-w-md mx-auto py-6 border-black pb-4">
               <input type="text" placeholder="Mot de passe" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
               <input type="text" placeholder="Confirmer mot de passe" className="bg-gray-200 placeholder-gray-500 border-b-2 border-black pb-4 w-4/5"/>
           </div>
-          <div className="flex py-5">
+          <div className="flex py-6">
           <input type="checkbox" name="role_choice" id="user" className="mx-6"/>
           <p className="text-gray-600">En soumettant ce formulaire, j'accepte les conditions d'utilisation de la plateforme. Voir le réglement.</p> 
           </div>
