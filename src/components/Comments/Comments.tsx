31976ebfd64bd955da22f2d9865532e391901761
NicolasMouret <mouret.nicolas63@gmail.com>
2023-09-29 19:28:09 +0200

    ajout de commentaire dynamique mais probleme de scroll

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,40 +1,66 @@
 "use client";
 
 import InputEmoji from 'react-input-emoji'
-import Image from 'next/image'
-import SignalButton from '@/src/components/Buttons/SignalButton'
-import { useState } from 'react';
+import Comment from './Comment'
+import { useState, useEffect, useRef } from 'react';
 
-function Comment() {
-  return (
-    <div className="flex gap-4 ">
-      <Image
-      className="rounded-full h-fit"
-      src="https://picsum.photos/id/500/360/350"
-      alt="Picture of the author"
-      width={50}
-      height={50}
-      />
-      <div className="flex flex-col max-w-[70%] ">
-          <p>Fred - 29 août 2023</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corrupti recusandae voluptas ullam perspiciatis optio illum omnis eligendi</p>
-      </div>
-      <SignalButton className="ml-auto text-left w-2/12 max-h-8" />
-    </div>
-    )
-}
-
+const commentsRawList = [
+  { avatar: "https://picsum.photos/id/500/360/350",
+    nickname: "Fred",
+    date: "29 Août 2023",
+    content: "Superbe !" },
+  { avatar: "https://picsum.photos/id/501/360/350",
+    nickname: "Bob",
+    date: "2 Septembre 2023",
+    content: "Pas mal, mais pas ma préférée de cet artiste." },
+  { avatar: "https://picsum.photos/id/502/360/350",
+    nickname: "Peter Parker",
+    date: "3 Septembre 2023",
+    content: "Très laid..." },
+  { avatar: "https://picsum.photos/id/503/360/350",
+    nickname: "Fred",
+    date: "29 Août 2023",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates libero perferendis molestiae accusamus, consequuntur autem aliquam maiores debitis fugiat voluptatem!" },
+  { avatar: "https://picsum.photos/id/504/360/350",
+    nickname: "Bob",
+    date: "2 Septembre 2023",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates libero perferendis molestiae accusamus." },
+  { avatar: "https://picsum.photos/id/505/360/350",
+    nickname: "Peter Parker",
+    date: "3 Septembre 2023",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates libero perferendis molestiae accusamus, consequuntur autem aliquam maiores debitis fugiat voluptatem!" },   
+]
 
 export default function CommentsBlock() {
   const [text, setText] = useState("");
+  const commentsContainerRef = useRef<HTMLDivElement>(null);
+
+  const commentsList = commentsRawList.map((comment) => {
+    return (
+      <Comment
+        key={comment.avatar}
+        avatar={comment.avatar}
+        nickname={comment.nickname}
+        date={comment.date}
+        content={comment.content}
+      />
+    )
+  })
+
+  const onEnter = (text: string) => {
+    commentsRawList.push(
+  { avatar: "https://picsum.photos/id/506/360/350",
+    nickname: "Utilisateur test",
+    date: "29 Septembre 2023",
+    content: text },
+    )
+    commentsContainerRef.current?.scrollTo(0, 1000);
+  }
+
   return (
     <div className="flex flex-col justify-between overflow-hidden pt-4 h-[30vh] w-[600px] rounded-2xl border-gray-400 border-2">
-      <div className="flex flex-col justify-between gap-2 overflow-auto h-[75%] pl-6 py-2">
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
+      <div ref={commentsContainerRef} className="flex flex-col-reverse justify-between gap-4 overflow-auto h-[75%] pl-6 py-2">
+        {commentsList}
       </div>
       <div className="flex max-h-[20%] w-[95%] mx-auto border-gray-300 border-t-2">
       <InputEmoji
@@ -45,13 +71,9 @@ export default function CommentsBlock() {
           placeholder="Ajoutez un commentaire"
           aria-label="Ajoutez un commentaire"
           language='fr'
+          onEnter={onEnter}
           shouldReturn
         />
-      {/* <textarea className="w-[95%] mx-auto justify-self-end border-gray-300 border-t-2 resize-none outline-none"
-      name="commentInput"
-      rows={2} 
-      placeholder="Ajoutez un commentaire" 
-      aria-label="Ajoutez un commentaire"></textarea> */}
       </div>
     </div>
     )
