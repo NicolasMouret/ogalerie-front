86e1c2d80047c592103417efe7097eae7f8f3cdf
NicolasMouret <mouret.nicolas63@gmail.com>
2023-10-04 22:55:55 +0200

    correction searchbar pour déploiement

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,12 +6,6 @@ import { AiOutlineSearch } from 'react-icons/ai';
 export default function SearchBar() {
     const [searchQuery, setSearchQuery] = useState('');
 
-    const handleInputChange = (e) => {
-        const value = e.target.value;
-        setSearchQuery(value)
-        console.log("Search Query:", value);
-      };
-
     return (
         <div className="flex items-center border-2 rounded-full border-gray-600 relative w-full sm:w-96">
             <input
@@ -19,7 +13,7 @@ export default function SearchBar() {
                 className="text-center py-1 sm:py-2 px-3 sm:px-4 text-sm sm:text-lg w-full outline-none rounded-full bg-transparent pl-3 sm:pl-4 pr-10"
                 placeholder="Rechercher"
                 value={searchQuery}
-                onChange={handleInputChange}
+                onChange={(e) => setSearchQuery(e.target.value)}
             />
             <AiOutlineSearch className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600 w-5 h-5" />
         </div>
