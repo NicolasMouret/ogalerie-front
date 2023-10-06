ecee4712256c9b4edd2063c4c90e942660180477
Aliénor Berthinier <128698817+AlienorBerthinier@users.noreply.github.com>
2023-10-06 11:24:16 +0200

    Alienor/reglement et cgu (#76)
    * création page à propos (responsive)
    
    * creation page reglement et cgu

---
+++ src/app/demos/reglement/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function WebsiteRulesPage() {
+
+  return (
+    <div className="pt-60 flex flex-col items-center justify-center pt-4 sm:pt-20 md:pt-0 h-[70vh] lg:mt-0">
+        <h1 className="pl-4 pr-4 text-center text-xl lg:text-2xl font-bold pb-4 lg:pb-10 mt-20 sm:mt-0">
+            Règlement de la plateforme & mentions légales
+        </h1>
+        <p className="text-justify max-w-lg lg:max-w-5xl mx-4 lg:mx-auto indent-4 lg:indent-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,
+            dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
+            molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a,
+            enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.<br /><br />
+            Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non
+            tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur
+            aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat
+            non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae
+            erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.
+        </p>
+    </div>
+    );
+}
