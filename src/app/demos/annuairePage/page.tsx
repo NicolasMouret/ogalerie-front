44344bbc051953d4c4121de3db2efaf2638ed15f
Aliénor Berthinier <128698817+AlienorBerthinier@users.noreply.github.com>
2023-10-06 16:39:03 +0200

    Alienor/search bar fonctionnelle (#78)
    * ajout logique barre de recherche annuaire
    
    * search bar annuaire artistes fonctionnelle

--- src/app/demos/annuairePage/page.tsx
+++ src/app/demos/annuairePage/page.tsx
@@ -3,34 +3,30 @@
 import { useState, useEffect } from 'react';
 import axiosInstance from '@/src/utils/axios';
 import AlphabetFilter from '@/src/components/AlphabetFilter/AlphabetFilter';
-import SearchBar from '../../../components/SearchBar/SearchBar';
+import SearchBarAnnuaire from '@/src/components/SearchBarAnnuaire/SearchBarAnnuaire';
+
+
 
 export default function Annuaire() {
   const [artists, setArtists] = useState([]);
-  const [filteredArtists, setFilteredArtists] = useState([])
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     axiosInstance.get('/users/creator')
         .then(response => {
             console.log(response.data)
-            setArtists(response.data);
-            setFilteredArtists(response.data); 
+            setArtists(response.data); 
         })
         .catch(error => {
             console.error("Erreur lors de la récupération des artistes:", error);
         });
 }, []);
 
-//   const handleLetterSelect = (letter: string) => {
-//     const filtered = artists.filter(artist => artist.nickname[0].toUpperCase() === letter);
-//     console.log("Filtered Artists by Letter:", filtered); 
-//     setFilteredArtists(filtered);
-//   };
-
-//   const filteredBySearch = filteredArtists.filter((artist) =>
-//   artist.nickname.toLowerCase().includes(searchQuery.toLowerCase())
-// );
+const filteredArtists = artists.filter((artist) =>
+    artist.nickname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    artist.firstname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    artist.lastname.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <>
@@ -39,10 +35,20 @@ export default function Annuaire() {
           Annuaire des artistes
         </h1>
         <div className="pb-3">
-          <SearchBar />
+          <SearchBarAnnuaire searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         </div>
         <AlphabetFilter />
-        
+    
+        {searchQuery && filteredArtists.length > 0 && (
+          <div className="flex flex-col md:flex-row gap-5 results-container mt-5">
+            {filteredArtists.map((artist) => (
+              <div key={artist.id} className="flex flex-col items-center">
+                <img src={artist.avatar} alt={artist.nickname} className="w-24 h-24 rounded-full"/>
+                <span>{artist.nickname}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </main>
     </>
   );
