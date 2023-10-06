2cbe933fc8ccb7bd77e63882e31629dc1aad7895
SostellTODA <sostelltoda@gmail.com>
2023-10-06 15:58:36 +0200

    sostell/cgu-rgpd

---
+++ src/app/demos/cguPolitiqueConfidentialite/page.tsx
@@ -0,0 +1,55 @@
+
+export default function submission_policies () {
+    return (
+  <>
+    <p className="mt-32">Cette Politique de confidentialité décrit comment O'Galerie collecte, utilise et protège les informations personnelles que vous nous fournissez lorsque vous utilisez notre site Web.</p>
+
+<h2 className="text 9xl m-16 font-extrabold uppercase">Collecte de données personnelles</h2>
+
+<p>Nous collectons les données personnelles suivantes lorsque vous utilisez notre site Web :</p>
+
+<ul>
+    <li>Nom et prénom</li>
+    <li>Adresse e-mail</li>
+    <li>Adresse postale</li>
+    <li>Numéro de téléphone</li>
+    <li>Informations de paiement (le cas échéant)</li>
+</ul>
+
+<h2 className="text 9xl m-16 font-extrabold uppercase">Utilisation des données personnelles</h2>
+
+<p>Nous utilisons vos données personnelles pour les finalités suivantes :</p>
+
+<ul>
+    <li>Vous fournir les produits ou services demandés</li>
+    <li>Vous envoyer des informations, des newsletters ou des offres spéciales</li>
+    <li>Améliorer notre site Web et nos services</li>
+</ul>
+
+<h2 className="text 9xl m-16 font-extrabold uppercase">Protection des données personnelles</h2>
+
+<p>Nous prenons des mesures pour protéger vos données personnelles et assurer leur sécurité. Vos informations ne seront pas partagées, vendues ou louées à des tiers sans votre consentement préalable.</p>
+
+<h2>Vos droits</h2>
+
+<p>Vous avez le droit d'accéder à vos données personnelles, de les rectifier, de les supprimer, de limiter leur traitement et de les transférer. Vous pouvez exercer ces droits en nous contactant à [adresse e-mail de contact].</p>
+
+<h2 className="text 9xl m-16 font-extrabold uppercase">Cookies</h2>
+
+<p>Notre site Web utilise des cookies pour améliorer votre expérience. En continuant à utiliser notre site, vous consentez à notre utilisation de cookies. Pour en savoir plus, consultez notre <a href="/politique-de-cookies">Politique sur les cookies</a>.</p>
+
+<h2 className="text 9xl m-16 font-extrabold uppercase">Contact</h2>
+
+<p>Pour toute question concernant notre Politique de confidentialité, veuillez nous contacter à [adresse e-mail de contact] ou à l'adresse postale suivante :</p>
+
+<ul>
+    <li>Nom de votre entreprise</li>
+    <li>Adresse postale</li>
+    <li>Code postal, Ville</li>
+    <li>Pays</li>
+</ul>
+
+<p>Cette Politique de confidentialité est en vigueur à partir du [date d'entrée en vigueur]. Toute modification de cette politique sera publiée sur cette page.</p>
+  </>
+    );
+  }
---
+++ src/app/demos/cguSubmissionPolicies/page.tsx
@@ -0,0 +1,32 @@
+
+export default function PolitiqueConfidentialite () {
+    return (
+  <>
+      <h2 className="text 9xl m-16 font-extrabold uppercase">Limitations des titres de ma collection et de ma galerie :</h2>
+    <p>Les membres d'O’Galerie peuvent créer d'incroyables collections de favoris et organiser leurs propres galeries. Une fonctionnalité importante est la possibilité de créer des titres personnalisés.</p>
+    <p>Les titres choisis pour ces galeries et collections doivent respecter les politiques d'O’Galerie et ne doivent pas être délibérément offensants ou inappropriés.</p>
+    <p>Les titres de galerie ou de collection qui enfreignent les normes de la communauté O’Galerie peuvent être réinitialisés par notre équipe de modération, et le profil responsable peut potentiellement faire l'objet de mesures disciplinaires.</p>
+
+    <h2 className="text 9xl m-16 font-extrabold uppercase">Directives de politique concernant les commentaires, les journaux, les statuts et les interactions générales :</h2>
+    <p>Les directives discutées ici s'appliquent à toutes les zones et méthodes d'interaction avec les autres membres d'O’Galerie. O’Galerie offre à tous les membres la possibilité de publier des commentaires, des entrées de journal personnel, des descriptions de déviation, des publications de statut et d'autres formes d'interaction.</p>
+
+    <h3 className="text 9xl m-16 font-extrabold uppercase">Directives générales :</h3>
+    <p>Comme pour nos autres politiques et pratiques liées à l'activité sociale au sein de la communauté O’Galerie, les membres sont autorisés à une liberté maximale d'expression pour le contenu publié dans ces zones, à condition que les publications puissent être considérées comme répondant aux attentes du public très vaste et diversifié attiré par O’Galerie en ce qui concerne le comportement socialement acceptable.</p>
+    <p>Nous essayons de tracer une ligne entre l'expression ouverte et libre, sous toutes ses formes, et la reconnaissance que de nombreuses personnes peuvent ne pas souhaiter être exposées à une activité excessivement perturbatrice, agressive, sexuellement explicite ou insensible sur O’Galerie.</p>
+    <p>En conséquence, bien que l'utilisation de la grossièreté ou d'un langage similaire soit tolérée lorsqu'elle est utilisée dans une conversation ou dans le cadre d'une soumission créative, les membres ne doivent pas faire des déclarations racistes, bigotes ou contenant d'autres formes de discours haineux.</p>
+    <p>L'utilisation de l'un des outils de communication mis à votre disposition dans le but de propager des discours haineux ou un comportement délibérément agressif ou abusif peut entraîner des mesures disciplinaires à l'encontre de votre profil O’Galerie.</p>
+    <p>Les membres ne sont pas autorisés à s'engager dans des conversations qui pourraient être considérées comme obscènes, sexuellement explicites ou pornographiques. Les conversations axées sur des sujets sexuels, mettant un accent démesuré sur les organes génitaux ou décrivant graphiquement des actes sexuels en détail peuvent entraîner des mesures disciplinaires à l'encontre de votre profil O’Galerie.</p>
+
+    <h3 className="text 9xl m-16 font-extrabold uppercase">Spam et publicité :</h3>
+    <p>Les entreprises commerciales sont invitées à contacter O’Galerie pour promouvoir leurs produits via des campagnes de marque. Pour plus d'informations, veuillez consulter la page des partenariats d'O’Galerie.</p>
+    <p>La création de comptes dans le but de faire de la publicité gratuite ou de tenter de mettre en œuvre des stratégies d'optimisation pour les moteurs de recherche sur O’Galerie est interdite. Toute personne tentant de le faire verra ses comptes fermés et ses sites Web potentiellement blacklistés.</p>
+
+    <h2 className="text 9xl m-16 font-extrabold uppercase">Politique concernant les messages privés (notes) :</h2>
+    <p>O’Galerie offre à tous les membres la possibilité d'envoyer des communications privées à d'autres membres via des notes.</p>
+    <p>Même si les notes sont des communications privées, elles sont toujours soumises à toutes les politiques et pratiques d'O’Galerie liées à l'activité sociale au sein de la communauté O’Galerie.</p>
+    <p>Comme pour d'autres zones où l'interaction sociale est possible, les membres sont autorisés à une liberté maximale d'expression, à condition que le contenu de la note puisse être considéré comme répondant aux attentes du public très vaste et diversifié attiré par O’Galerie.</p>
+    <p>Les politiques et pratiques d'O’Galerie cherchent à tracer une ligne entre l'expression ouverte et libre, sous toutes ses formes, et la reconnaissance que de nombreuses personnes peuvent ne pas souhaiter être exposées à une activité excessivement perturbatrice, agressive, sexuellement explicite ou insensible sur O’Galerie.</p>
+    <p>Les membres ne sont pas autorisés à utiliser les notes pour faire des déclarations racistes, bigotes ou contenant d'autres formes de discours haineux, ni à inclure des communications qui pourraient être considérées comme obscènes, explicites ou constituant du harcèlement sexuel.</p>
+  </>
+    );
+  }
