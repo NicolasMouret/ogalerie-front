fdd1550fc2e97be4062c7d27fbb8afda2e9540e5
NicolasMouret <mouret.nicolas63@gmail.com>
2023-10-10 09:31:44 +0200

    correction authorisation images

--- src/components/Forms/AuthentificationForm.tsx
+++ src/components/Forms/AuthentificationForm.tsx
@@ -59,7 +59,10 @@ export default function AuthentificationForm() {
     const form = event.currentTarget;
     const formData = new FormData(form);
     const objData = Object.fromEntries(formData);
-    objData.avatar = uploadUrl;
+    
+    if (uploadUrl) {
+      objData.avatar = uploadUrl;
+    }
     console.log("Données du formulaire saisies:", objData);
 
     // Validation checks
