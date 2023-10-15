24c91d029db491c33cb488d415af68f9e2b99717
SostellTODA <sostelltoda@gmail.com>
2023-10-15 17:19:34 +0200

    sostell/responsiveCGU

--- src/app/cgu-page/page.tsx
+++ src/app/cgu-page/page.tsx
@@ -4,28 +4,27 @@ import Link from 'next/link';
 export default function WebsiteRulesPage() {
   return (
     <div
-      className="flex flex-col items-center justify-center pt-4 sm:pt-20 md:pt-0 h-[70vh] lg:mt-0 bg-background bg-cover bg-center"
-      style={{ backgroundImage: 'url(\'/images/background_image_a_propos.png\')' }}
+      className="flex flex-col items-center justify-center h-[70vh] border-black border-t-4 border-4"
     >
-      <h1 className="pl-4 pr-4 text-center text-xl lg:text-4xl font-bold pb-4 lg:pb-10 mt-20 sm:mt-0">
+      <h1 className="text-center font-bold pb-4 mt-2 lg:mt-20 text-lg sm:text-2xl md:text-3xl xl:text-4xl">
         Règlement de la plateforme & mentions légales
       </h1>
       <div>
-        <ul className="text-justify max-w-lg lg:max-w-5xl mx-4 lg:mx-auto indent-4 lg:indent-8 text-xl">
-          <li className="flex p-6 text-3xl">
+        <ul className="text-xl">
+          <li className="flex p-6 text-md sm:text-xl md:text-2xl xl:text-3xl">
             <IoIosArrowDroprightCircle />
             {' '}
             <Link href="./cgu-page/general-policy" className="hover:underline hover:text-black-500">Politiques Générales</Link>
           </li>
-          <li className="flex p-6 text-3xl">
+          <li className="flex p-6 text-md sm:text-xl md:text-2xl xl:text-3xl">
             <IoIosArrowDroprightCircle />
             <Link href="./cgu-page/submission-policy" className="hover:underline hover:text-black-500">Politiques de Soumission</Link>
           </li>
-          <li className="flex p-6 text-3xl">
+          <li className="flex p-6 text-md sm:text-xl md:text-2xl xl:text-3xl">
             <IoIosArrowDroprightCircle />
             <Link href="./cgu-page/community-etiquette" className="hover:underline hover:text-black-500">Étiquette de la Communauté</Link>
           </li>
-          <li className="flex p-6 text-3xl">
+          <li className="flex p-6 text-md sm:text-xl md:text-2xl xl:text-3xl">
             <IoIosArrowDroprightCircle />
             <Link href="./cgu-page/privacy-policy" className="hover:underline hover:text-black-500">Politique de Confidentialité</Link>
           </li>
